fe21cab1eb492300daaaabceb2d68b3f1f5dd17c
Pascal <pascal@pascal.com>
2024-03-29 17:24:28 -0400

    rename target

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "current-color",
+    name: "current-desktop-color",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "current-color"),
+            name: "current-desktop-color"),
     ]
 )
