d792a9111ba9b4a9eb79ecd8db7c064991b34d5b
Pascal <pascal@pascal.com>
2024-03-29 22:30:14 -0400

    Update main.swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,6 +4,8 @@ import AppKit
 let version = ProcessInfo.processInfo.operatingSystemVersion
 let options = NSWorkspace.shared.desktopImageOptions(for: NSScreen.main!)!
 
+print("Thanks! Please email the info below to pascalpp+fillcolor@gmail.com")
+print("")
 print("macOS version:", "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)")
 print("imageScaling:", options[.imageScaling])
 print("fillColor:", options[.fillColor])
